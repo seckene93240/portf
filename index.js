eebefe2ed3126717c45d5374303c0ca4e9edd18f
seckene93240 <sackene.sako@lepoles.org>
2025-03-05 17:00:28 +0100

    deux7

--- index.js
+++ index.js
@@ -4,22 +4,21 @@ let spé1=document.querySelector(".spé1")
 let liste1=document.querySelector(".liste1")
 let button=document.querySelector("button")
 let li =document.querySelector("li")
-
+let projet=document.querySelector(".projet")
+let projet1=document.querySelector(".projet1")
 let gauche=document.querySelector(".gauche")
 let droit =document.querySelector(".droit")
 let boss =document .querySelector(".boss")
+let chef =document .querySelector(".chef")
+let chef2 =document .querySelector(".chef2")
 burger.addEventListener("click",()=>{
         
     spé1.style.display="block"
     burger.style.display="none"
     spé1.style.zIndex = "1";
 
-    liste1.style.marginTop = "-4%";
-    droit.style.display="none"
-    gauche.style.display="none"
-    boss.style.display="none"
-   li.style.marginTop="100%"
-
+   
+  
 })
 
 
@@ -33,7 +32,27 @@ button.addEventListener("click",()=>{
     droit.style.display="flex"
     gauche.style.display="flex"
     boss.style.display="flex"
-    burger.style.display="flex"
+    projet.style.display="flex"
+    projet1.style.display="flex"
+
+    projet.style.justifyContent = 'center';
+    projet.style.alignItems = 'center'
+ if (window.innerWidth < 900) {
+       burger.style.display="block"
+ }
+ else{
+     burger.style.display="flex"
+ }
+})
+window.addEventListener('resize', () => {
+    if (window.innerWidth < 1000) {
+        burger.style.display = "block";
+        spé1.style.display="none"
+        
+    } else {
+        burger.style.display = "flex";
+          spé1.style.display="flex"
+    }
 })
 
 
