ee220eb853ac6be4cd23282dee9e542b58d43816
seckene93240 <seckene.sako@lepoles.org>
2025-06-20 15:07:03 +0200

    first commit656546554662

--- index.js
+++ index.js
@@ -1,70 +1,16 @@
-let burger = document.querySelector(".haha")
-let spé1=document.querySelector(".spé1")
+const burger = document.getElementById("burger");
+const closeBtn = document.getElementById("closeBtn");
+const navLinks = document.getElementById("navLinks");
 
-let liste1=document.querySelector(".liste1")
+burger.addEventListener("click", () => {
+  navLinks.classList.add("active");
+  closeBtn.classList.add("active");
+  burger.classList.add("hide");
+});
 
-let li =document.querySelector("li")
-let projet=document.querySelector(".projet")
-let projet1=document.querySelector(".projet1")
-let gauche=document.querySelector(".gauche")
-let droit =document.querySelector(".droit")
-let boss =document .querySelector(".boss")
-let chef =document .querySelector(".chef")
-let chef2 =document .querySelector(".chef2")
-let body =document.querySelector("body")
-let ds3 =document.querySelector(".ds3")
-burger.addEventListener("click",()=>{
-        
-    spé1.style.display="block"
-    burger.style.display="none"
-    spé1.style.zIndex = "1";
-ds3.style.display="flex"
-boss.style.display="none"
-droit.style.marginTop = '30%';
-})
-
-
-ds3.addEventListener("click",()=>{
-    spé1.style.display="none"
-    burger.style.display = "flex";
-    ds3.style.display="none"
-    boss.style.display="flex"
-    droit.style.marginTop = '10%';
-})
-
-button.addEventListener("click",()=>{
- 
-      
-    liste1.style.marginTop = "3%";
-    li.style.marginTop="0%"
-    droit.style.display="flex"
-    gauche.style.display="flex"
-    boss.style.display="flex"
-    projet.style.display="flex"
-    projet1.style.display="flex"
-
-    projet.style.justifyContent = 'center';
-    projet.style.alignItems = 'center'
- if (window.innerWidth < 1000) {
-       burger.style.display="flex"
- }
- else{
-     burger.style.display="none"
- }
-
-
-})
-
-
-window.addEventListener('resize', () => {
-    if (window.innerWidth < 1000) {
-        burger.style.display = "flex";
-        spé1.style.display="none"
-        
-    } else {
-        burger.style.display = "flex";
-          spé1.style.display="flex"
-    }
-   
-})
+closeBtn.addEventListener("click", () => {
+  navLinks.classList.remove("active");
+  closeBtn.classList.remove("active");
+  burger.classList.remove("hide");
+});
 
